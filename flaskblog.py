4cb8166de57fd8651baee657e91273e651d0ea17
Ryoung27 <RRYoung89@gmail.com>
2019-10-08 09:08:35 -0500

    form validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e736f275f88a4e881c23a1e0508c64db'
 
 posts = [
     {
@@ -26,6 +29,17 @@ def home():
 def about():
     return render_template('about.html', title="About")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+    return render_template('register.html', title='Login', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
